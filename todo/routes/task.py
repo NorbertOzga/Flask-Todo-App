a329a93627c7bba99efdcb04bf67701cf3adac3f
Norbert Ozga <norbert.ozga@cervirobotics.com>
2020-10-16 10:45:11 +0200

    Create routes skeleton for tasks

--- todo/routes/task.py
+++ todo/routes/task.py
@@ -1,8 +1,24 @@
-from flask import jsonify
+from flask import request
 
 from . import bp
 
 
+@bp.route('/task', methods=['GET', 'POST'])
+def task():
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+
+
+@bp.route('/task/<int:id>', methods=['GET', 'DELETE'])
+def task_id():
+    if request.method == 'GET':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
+
 @bp.route('/task/all', methods=['GET'])
 def task_all():
-    return jsonify(['123', '345']), 200
+    pass
