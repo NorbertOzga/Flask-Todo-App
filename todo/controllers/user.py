5f8668e8f88a63fff660375e9e3fab9d73ab38fc
Norbert Ozga <norbert.ozga@cervirobotics.com>
2020-10-22 12:45:20 +0200

    Add schema dumping in user controllers

--- todo/controllers/user.py
+++ todo/controllers/user.py
@@ -10,6 +10,7 @@ from todo.models.user import User, user_schema
 
 def get_user(user_id: int) -> tp.Tuple[tp.Dict, int]:
     data = User.query.get_or_404(user_id)
+
     return {"status": "success", "data": user_schema.dump(data)}, HttpStatus.OK
 
 
@@ -20,7 +21,7 @@ def create_user(json_data) -> tp.Tuple[tp.Dict, int]:
     try:
         data = user_schema.load(json_data)
     except ValidationError as e:
-        return ({"success": "fail", "data": f"{e.messages}"},
+        return ({"status": "fail", "data": f"{e.messages}"},
                 HttpStatus.UNPROCESSABLE_ENTITY)
 
     new_user = User(**data)
@@ -32,13 +33,14 @@ def create_user(json_data) -> tp.Tuple[tp.Dict, int]:
         return ({"status": "fail", "message": e.code},
                 HttpStatus.INTERNAL_SERVER_ERROR)
 
-    return {"status": "success", "data": new_user}, HttpStatus.CREATED
+    return ({"status": "success", "data": user_schema.dump(new_user)},
+            HttpStatus.CREATED)
 
 
 def delete_user(user_id: int) -> tp.Tuple[tp.Dict, int]:
     user = User.query.filter(User.id == user_id)
     if user.count() == 0:
-        return ({"status": "fail", "message": "User do not exists"},
+        return ({"status": "fail", "message": "User does not exists"},
                 HttpStatus.BAD_REQUEST)
 
     user.delete()
@@ -47,4 +49,5 @@ def delete_user(user_id: int) -> tp.Tuple[tp.Dict, int]:
     except exc.SQLAlchemyError as e:
         return ({"status": "fail", "message": e.code},
                 HttpStatus.INTERNAL_SERVER_ERROR)
+
     return {"status": "success", "data": None}, HttpStatus.OK
