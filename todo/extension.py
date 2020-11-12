12ae4982741090e9780e3ca239a9ccd73b0529e6
Norbert Ozga <norbert.ozga@cervirobotics.com>
2020-11-12 14:04:58 +0100

    Add jwt token

--- todo/extension.py
+++ todo/extension.py
@@ -1,7 +1,9 @@
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 database = SQLAlchemy()
 marshmallow = Marshmallow()
 migrate = Migrate()
+jwt = JWTManager()
